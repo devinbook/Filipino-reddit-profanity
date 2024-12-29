54c12a907e53457849add3efafd3ed1d16a09577
devinbook <63249997+devinbook@users.noreply.github.com>
2024-12-29 23:37:46 +0800

    Update reddit_api.py

--- reddit_api.py
+++ reddit_api.py
@@ -1,16 +1,18 @@
 import praw
 
 def fetch_reddit_comments(post_url):
-    reddit = praw.Reddit(client_id='uLfq7N-Kp-n_noaXUDVuTg',
-                         client_secret='IUhw5TKGpWeRI2yweNOEwuzeIVVi8A',
-                         user_agent='Profanity_tagalog')
+    try:
+        reddit = praw.Reddit(client_id='fl_6wvPC8PcnJ4buzYHePA',
+                     client_secret='p_Z28aBGbePUpH7tJSTvVl9V0cbqRw',
+                     user_agent='Profanity_tagalog_dataset')
+        submission = reddit.submission(url=post_url)
+        submission.comments.replace_more(limit=0)
 
-    submission = reddit.submission(url=post_url)
-    submission.comments.replace_more(limit=0) 
-    comments = [
-        comment.body 
-        for comment in submission.comments.list() 
-        if comment.author and comment.author.name != 'AutoModerator' and not comment.stickied
-    ]
-    
-    return comments
+        comments = []
+        for comment in submission.comments.list():
+            if comment.author and comment.author.name != 'AutoModerator':
+                comments.append({"user": comment.author.name, "text": comment.body})
+        return comments
+    except Exception as e:
+        print(f"Error fetching comments: {e}")
+        return []
